5b4ea2e4d32eb5514f658b5dd6433f1c8f197204
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-05-09 13:46:03 +0300

    09.05.2021

---
+++ MVCProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules.FluentValidation;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        // GET: AdminCategory
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var categoryValues = categoryManager.GetList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult result = categoryValidator.Validate(category);
+            if(result.IsValid)
+            {
+                categoryManager.CategoryAdd(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
